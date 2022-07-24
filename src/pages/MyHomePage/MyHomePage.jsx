be0b20fd10ad9d72e95f1b1e07695cc15689eba1
mileslathan <mlathan.cn@gmail.com>
2022-07-24 12:58:01 -0400

    First commit to initialize project.

---
+++ src/pages/MyHomePage/MyHomePage.jsx
@@ -0,0 +1,5 @@
+export default function MyHomePage() {
+    return (
+        <h1>This is the Home Route. ☕️</h1>
+    )
+}
---
+++ src/pages/MyRecordsPage/MyRecordsPage.jsx
@@ -0,0 +1,5 @@
+export default function MyRecordsPage() {
+    return (
+        <h1>This is my records page babe!</h1>
+    )
+}
