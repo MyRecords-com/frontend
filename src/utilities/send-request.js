ba5876ce08d8a428c6526fba24ce7e258409df95
mileslathan <mlathan.cn@gmail.com>
2022-07-25 17:32:24 -0400

    Working on API for users profile data.

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -1,3 +1,24 @@
-export default async function sendRequest(){
+import axios from "axios";
+
+export default async function sendRequest(url, method='GET'){
     
+ let data;
+
+const options = {
+  method: 'GET',
+  url: 'https://omgvamp-hearthstone-v1.p.rapidapi.com/info',
+  headers: {
+    'X-RapidAPI-Key': 'ac0eb5e49amshb93c4156f9579bep1f0f93jsna1d1162d60af',
+    'X-RapidAPI-Host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
+  }
+};
+
+const res = await axios.request(url, options);
+    if (res.ok) return res.json();
+    throw new Error('Bad Request')
+
+    // const res = await fetch(url, options);
+    // // res.ok will be false if the status code set to 4xx in the controller action
+    // if (res.ok) return res.json();
+    // throw new Error('Bad Request');
 }
