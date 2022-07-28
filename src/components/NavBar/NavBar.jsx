7a30eaf1a9987858e8f4917d56bde4e2c806ec9e
Miles Lathan <mlathan.cn@gmail.com>
2022-07-28 15:33:54 -0400

    added login in navbar.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,15 @@
 import profileimg from '../../img/img_avatar.png'
+import AuthContext from "../../context/AuthContext";
+import { useContext } from 'react'
+import LoginPage from '../LoginPage/LoginPage';
+
 import './NavBar.css'
 export default function NavBar() {
     
+  const { user } = useContext(AuthContext);
+
+
+if (user === null) {
     return (
 <nav className="navbar navbar-dark bg-dark fixed-top">
   <div className="container-fluid">
@@ -20,8 +28,7 @@ export default function NavBar() {
             <a className="nav-link" href="#"><img src="" alt="" /></a>
             </li>
           <li className="nav-item text-center">
-            <a className="nav-link" href="#"><img src={profileimg} className="avatar"></img></a><br></br>
-            Welcome Back Miles !
+          <LoginPage />
           </li>
           <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="#">Home</a>
@@ -51,4 +58,56 @@ export default function NavBar() {
   </div>
 </nav>
     )
+} else {
+  return (
+    <nav className="navbar navbar-dark bg-dark fixed-top">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">MyRecords</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Navigation</h5>
+            <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li className="nav-item">
+                <a className="nav-link" href="#"><img src="" alt="" /></a>
+                </li>
+              <li className="nav-item text-center">
+                <a className="nav-link" href="#"><img src={profileimg} className="avatar"></img></a><br></br>
+                Welcome Back <b>{user.username}</b> !<br></br>
+                <button type="button" class="btn btn-primary btn-sm">Logout</button>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">My Dashboard</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Search Records</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu dropdown-menu-dark">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li>
+                    <hr className="dropdown-divider"></hr>
+                  </li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+        )
+}
 }
--- src/pages/MyHomePage/MyHomePage.jsx
+++ src/pages/MyHomePage/MyHomePage.jsx
@@ -1,10 +1,15 @@
 import axios from "axios";
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef, useContext } from 'react'
 import MyDashboard from "../MyDashboard/MyDashboard"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import * as ajunaAPI from '../../utilities/external-api'
 import Carousel from '../../components/Carousel/Carousel'
 import MainPageCard from "../../components/MainPageCard/MainPageCard";
+import UserInfo from '../../components/UserInfo/UserInfo'
+import AuthContext from "../../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+
 export default function MyHomePage() {
   
   let [ releases, setReleases ] = useState([])
@@ -20,7 +25,10 @@ export default function MyHomePage() {
         getReleases();
       }, []);
   
-      
+  
+  const { user } = useContext(AuthContext);
+
+
     return (
       <>
       <div className="container">
@@ -30,6 +38,7 @@ export default function MyHomePage() {
       <div className="container">
       <div>
         <h1>NEW RELEASES:</h1>
+        <UserInfo user={user} />
       </div>
       <div>
      {  
