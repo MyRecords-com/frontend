2cbe5ddc08bed126484665bdbd625f74f2b91b6b
Miles Lathan <mlathan.cn@gmail.com>
2022-07-31 11:47:52 -0400

    added data to the records page for the search query.

--- src/components/RecordCard/RecordCard.jsx
+++ src/components/RecordCard/RecordCard.jsx
@@ -1,5 +1,17 @@
 export default function RecordCard({rec}) {
     return (
-        <div>{rec.title}</div>
-    )
+      <>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm"><img src={rec.thumb}></img></div>
+            <div class="col-sm">{rec.title}</div>
+            <div class="col-sm">{rec.label}</div>
+            <div class="col-sm">{rec.year}</div>
+            <div class="col-sm">{rec.country}</div>
+            <div class="col-sm">{rec.style}</div>
+            <div class="col-sm"><button type="button" class="btn btn-primary btn-sm">Add to Collection</button></div>
+          </div>
+        </div>
+      </>
+    );
 }
